95bd8664feb4be48a8cd6ed78d25f5bb7f3aa3e4
alex6296 <32260519+alex6296@users.noreply.github.com>
2018-04-24 17:14:50 +0200

    updated lagdeling en smule

---
+++ src/business/caseOpening/IcaseOpening.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.caseOpening;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Lenovo
+ */
+public interface IcaseOpening {
+    
+    /**
+     * 
+     * @param declarationOfConsent
+     * @param consentRecieved
+     * @param citizenAcceptance
+     * @param citizenWithdrawn
+     * @param caseProgress
+     * @param caseBenefit
+     * @param caseOffer
+     * @return boolean to verify the case has been created
+     */
+   void CaseOpening(
+           String declarationOfConsent,
+           boolean consentRecieved,
+           boolean citizenAcceptance,
+           boolean citizenWithdrawn, 
+           int caseProgress,
+           int caseBenefit, 
+           int caseOffer);
+   
+   void addCaseInformation(Collection<String> information);
+           
+    
+}
