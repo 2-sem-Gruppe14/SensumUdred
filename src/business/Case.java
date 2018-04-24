4d33da294dd630cf88d4ed47c9bfde39cccbaacc
alex6296 <32260519+alex6296@users.noreply.github.com>
2018-04-24 20:02:17 +0200

    database stump og case information

--- src/business/Case.java
+++ src/business/Case.java
@@ -1,34 +1,28 @@
 package business;
 
-
-
 import java.sql.Timestamp;
 import java.util.Date;
 
-
 public class Case {
+
     /* case details */
-    public int caseID;
-    Timestamp creationDate;
-    public Date lastChanged;
-    public int lastChangedBy;
-    public String caseDescription;
+    private int caseID;
+    private Timestamp creationDate;
+    private Date lastChanged;
+    private int lastChangedBy;
+    private String caseDescription;
+    private int caseCreator;
 
     /* case information */
-     private String caseAssessment;
-    
+    private String caseAssessment;
+
     public Case(int caseID, int CaseWorkerID, String caseDescription) {
         this.caseID = caseID;
         this.creationDate = new Timestamp(System.currentTimeMillis());
         this.lastChanged = creationDate;
         this.lastChangedBy = CaseWorkerID;
         this.caseDescription = caseDescription;
+        this.caseCreator = CaseWorkerID;
     }
-    
-    
-    }
-   
-    
-        
-        
-    
+
+}
