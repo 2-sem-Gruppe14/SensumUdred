db2ed4ec13be83662dc9c81b68cf09a1a977aa3e
Jeanjohnsen <Geniusnh4@gmail.com>
2018-05-04 00:28:54 +0200

    SaveToFile printe di print scoop di woop di poop

--- src/data/SaveToFile.java
+++ src/data/SaveToFile.java
@@ -10,33 +10,60 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.sql.Timestamp;
 
 /**
  *
  * @author geniu
  */
-public class SaveToFile implements IData, Serializable {
+public class SaveToFile implements IData {
 
-    Timestamp Timestamp;
-    PreparedStatement ps = null;
-    Connection connection = null;
-    ResultSet rs = null;
     File logFile;
+    
     public SaveToFile(){
     
     }
     
-    private void writeToLog(int caseID, int workerID, String logType) throws SQLException, FileNotFoundException {
+    private void writeToLog(int caseID, int workerID, String logType, Timestamp Timestamp) throws FileNotFoundException {
         
-        
+        try(PrintStream out = new PrintStream("Log.txt")){
+
+            out.println(caseID);
+            out.println(workerID);
+            out.println(logType);
+            out.println(Timestamp);
 
         }
     }
+    
+    private void getLog() throws FileNotFoundException, IOException {
+        
+        try(BufferedReader br = new BufferedReader(new FileReader("Log.txt"))){
+        
+            String dataLine = null;
+            
+            while((dataLine = br.readLine()) != null){
+                System.out.println(dataLine);
+            }
+            
+        }
+        
+    }
+    
+    private void writeToCase(int caseID, String caseInformation, Timestamp Timestamp) throws FileNotFoundException{
+        
+        try(PrintStream out = new PrintStream("Case.txt")){
 
+            out.println(caseID);
+            out.println(caseInformation);
+            out.println(Timestamp);
+
+        }
+    }
+    
+    private void getCase() throws FileNotFoundException, IOException {
+        
+    }
 }
