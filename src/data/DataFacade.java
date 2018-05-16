e9b0041925d74b94fd1d02f45aa7baa613cbe440
JN97 <32257432+JN97@users.noreply.github.com>
2018-05-16 13:49:37 +0200

    Added String Queries for DataFacade

--- src/data/DataFacade.java
+++ src/data/DataFacade.java
@@ -50,69 +50,95 @@ public class DataFacade implements IData {
     //<editor-fold defaultstate="collapsed" desc="Case">
     @Override
     public Case getCase(int caseID) {
+        String Query = "SELECT * FROM Case WHERE "+caseID+" = Case_ID";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public int[] getCaseIDs(int CaseWorker) {
+        String Query = "SELECT Case_ID FROM Case WHERE "+CaseWorker+" = Case_ID";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void addCase(int caseID, int CPR, String caseContent) throws FileNotFoundException {
+        String Query = "INSERT INTO Case(Case_ID, CPR, caseContent) VALUES (Case, CPR, "+caseContent+")";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="log">
     @Override
-    public void saveLog(String logType, int WorkerID, int CaseID, Timestamp Creation) {
+    public void saveLog(String logType, int WorkerID, int CaseID, Timestamp Creation) { 
+        String Query = "INSERT INTO Log VALUES (logType, WorkerID, CaseID, Timestamp Creation)";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void showLog() {
+        String Query = "SELECT * FROM Log WHERE Log = Log_ID"; 
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String getCaseLog(int caseID) {
+        String Query = "SELECT * FROM Log WHERE "+caseID+" = Log_ID"; 
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void LogUserAdd(int createdUserID, int adminID) {
+        String Query = "INSERT INTO Log VALUES (createdUserID, "+adminID+")";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     //</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="user">
     public User getUser(int ID) {
+        String Query = "SELECT * FROM System_user WHERE "+ID+" = User_ID";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public User getUser(String username) {
+        String Query = "SELECT * FROM System_user WHERE "+username+" = User_ID";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public int getID(String Username) {
+    public int getID(String username) {
+        String Query = "SELECT ID FROM System_user WHERE "+username+" = User_ID";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public String getName(String Username) {
+    public String getName(String username) {
+        String Query = "SELECT name FROM System_user WHERE "+username+" = User_ID";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public String GetPassword(String username) {
+        String Query = "SELECT password FROM System_user WHERE "+username+" = User_ID";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void addUser(User User) {
+        String Query = "INSERT INTO System_user(User_ID, username, password, user_type) VALUES (User, "+User+")";
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     //</editor-fold>
